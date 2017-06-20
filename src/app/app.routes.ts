19eceda966de2f6536770721ee0af7c5f3558cab
Pablo Ibanez <me@pabloibanez.com>
2017-06-20 20:22:09 +0100

    Changes to make angular-cli to work on Heroku

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,6 @@ import {Routes} from '@angular/router';
 
 import {Dashboard1Component} from './views/dashboards/dashboard1.component';
 import {Dashboard2Component} from './views/dashboards/dashboard2.component';
-import {Dashboard3Component} from './views/dashboards/dashboard3.component';
-import {Dashboard4Component} from './views/dashboards/dashboard4.component';
-import {Dashboard41Component} from './views/dashboards/dashboard41.component';
 import {Dashboard5Component} from './views/dashboards/dashboard5.component';
 
 import {StarterViewComponent} from './views/appviews/starterview.component';
@@ -24,15 +21,12 @@ export const ROUTES: Routes = [
     children: [
       {path: 'dashboard1', component: Dashboard1Component},
       {path: 'dashboard2', component: Dashboard2Component},
-      {path: 'dashboard3', component: Dashboard3Component},
-      {path: 'dashboard4', component: Dashboard4Component},
       {path: 'dashboard5', component: Dashboard5Component}
     ]
   },
   {
     path: 'dashboards', component: TopNavigationLayoutComponent,
     children: [
-      {path: 'dashboard41', component: Dashboard41Component}
     ]
   },
   {
--- src/app/views/dashboards/dashboards.module.ts
+++ src/app/views/dashboards/dashboards.module.ts
@@ -1,12 +1,9 @@
-import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import {Dashboard1Component} from "./dashboard1.component";
-import {Dashboard2Component} from "./dashboard2.component";
-import {Dashboard3Component} from "./dashboard3.component";
-import {Dashboard4Component} from "./dashboard4.component";
-import {Dashboard41Component} from "./dashboard41.component";
-import {Dashboard5Component} from "./dashboard5.component";
+import { Dashboard1Component } from "./dashboard1.component";
+import { Dashboard2Component } from "./dashboard2.component";
+import { Dashboard5Component } from "./dashboard5.component";
 
 // Chart.js Angular 2 Directive by Valor Software (npm)
 import { ChartsModule } from 'ng2-charts/ng2-charts';
@@ -19,9 +16,9 @@ import { JVectorMapModule } from '../../components/map/jvectorMap';
 
 
 @NgModule({
-  declarations: [Dashboard1Component,Dashboard2Component,Dashboard3Component,Dashboard4Component,Dashboard41Component,Dashboard5Component],
-  imports     : [BrowserModule,ChartsModule, FlotModule,IboxtoolsModule,PeityModule,SparklineModule,JVectorMapModule],
-  exports     : [Dashboard1Component,Dashboard2Component,Dashboard3Component,Dashboard4Component,Dashboard41Component,Dashboard5Component],
+  declarations: [Dashboard1Component, Dashboard2Component, Dashboard5Component],
+  imports: [BrowserModule, ChartsModule, FlotModule, IboxtoolsModule, PeityModule, SparklineModule, JVectorMapModule],
+  exports: [Dashboard1Component, Dashboard2Component, Dashboard5Component],
 })
 
-export class DashboardsModule {}
+export class DashboardsModule { }
