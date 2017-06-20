7eeede288a4fbd649ef4c55717682128a30baf9c
Pablo Ibanez <me@pabloibanez.com>
2017-06-20 20:15:47 +0100

    Changes to make angular-cli to work on Heroku

--- src/app/components/common/layouts/layouts.module.ts
+++ src/app/components/common/layouts/layouts.module.ts
@@ -1,17 +1,17 @@
-import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
-import {RouterModule} from "@angular/router";
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
 
 import {BsDropdownModule} from 'ngx-bootstrap';
 
-import {BasicLayoutComponent} from "./basicLayout.component";
-import {BlankLayoutComponent} from "./blankLayout.component";
-import {TopNavigationLayoutComponent} from "./topNavigationlayout.component";
+import {BasicLayoutComponent} from './basicLayout.component';
+import {BlankLayoutComponent} from './blankLayout.component';
+import {TopNavigationLayoutComponent} from './topNavigationlayout.component';
 
-import {NavigationComponent} from "./../navigation/navigation.component";
-import {FooterComponent} from "./../footer/footer.component";
-import {TopNavbarComponent} from "./../topnavbar/topnavbar.component";
-import {TopNavigationNavbarComponent} from "./../topnavbar/topnavigationnavbar.component";
+import {NavigationComponent} from './../navigation/navigation.component';
+import {FooterComponent} from './../footer/footer.component';
+import {TopNavbarComponent} from './../topnavbar/topnavbar.component';
+import {TopNavigationNavbarComponent} from './../topnavbar/topnavigationnavbar.component';
 
 
 @NgModule({
